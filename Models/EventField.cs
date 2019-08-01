b539f034fe781dedd8c8d8dcf707144278a452ab
Taqwa Sleem <Taqwa-sleem@hotmail.com>
2019-08-01 12:08:33 +0300

    adding Models

---
+++ Models/EventField.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScreenProject.Models
+{
+    public class EventField
+    {
+        public int Id { get; set; }
+        public int EventId { get; set; }
+        public int TemplateFieldId { get; set; }
+        public int Value { get; set; }
+
+        public Event MyEvent { get; set; }
+        public TemplateField MyTemplateField { get; set; }
+
+
+    }
+}
---
+++ Models/Template.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScreenProject.Models
+{
+    public class Template
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BackGroundImg { get; set; }
+
+        public ICollection<TemplateField> TemplateFields { get; set; }
+        public ICollection<Event> Events { get; set; }
+
+    }
+}
