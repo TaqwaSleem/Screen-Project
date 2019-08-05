ee290437d55b9bff1d84e72013a97620af523fe3
Taqwa Sleem <Taqwa-sleem@hotmail.com>
2019-08-05 19:57:16 +0300

    app component

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+//import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
+//@Component({
+//  selector: 'app-root',
+//  templateUrl: './app.component.html',
+//  styleUrls: ['./app.component.css']
+//})
+//export class AppComponent {
+//  title = 'app';
+//}
+
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  styleUrls: ['./app.component.css']})
 export class AppComponent {
-  title = 'app';
+  public events: any[];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<any[]>(baseUrl + 'api/Event/GetAnnualMonthlyDailyAndOnce').subscribe(result => {
+      this.events = result;
+    }, error => console.error(error));
+  }
+}
+
+interface WeatherForecast {
+  dateFormatted: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+
+
 }
